830f56611e4b3464002bbd047ef3fc9a3f558d1d
ecfhl <daniel.r.caissie@gmail.com>
2026-09-24 10:19:10 -0300

    Show live MySQL connection status on homepage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    $databaseConnected = false;
+    $databaseVersion = null;
+    $databaseName = config('database.connections.mysql.database');
+
+    try {
+        $row = DB::selectOne('SELECT VERSION() AS version');
+        $databaseVersion = $row->version ?? null;
+        $databaseConnected = true;
+    } catch (\Throwable $e) {
+        report($e);
+    }
+
+    return view('home', compact('databaseConnected', 'databaseVersion', 'databaseName'));
 });
