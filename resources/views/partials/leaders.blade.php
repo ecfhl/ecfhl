31824bf9ae829924e30debc0b97015845e631f05
ecfhl <daniel.r.caissie@gmail.com>
2026-09-25 12:39:21 -0300

    Show five leader rows outside overview

--- resources/views/partials/leaders.blade.php
+++ resources/views/partials/leaders.blade.php
@@ -1,13 +1,14 @@
+@php $leaderLimit = $limit ?? 5; @endphp
 <div class="grid-3 leader-cards">
 @foreach($cards as $key=>$title)
 <article class="card leader-card">
 <h3 class="leader-card-title">{{ $title }}</h3>
-@forelse(array_slice($leaderRows[$key]??[],0,3) as $i=>$row)
+@forelse(array_slice($leaderRows[$key]??[],0,$leaderLimit) as $i=>$row)
 @include('partials.leader-row')
 @empty<div class="empty">No recorded results for this selection.</div>@endforelse
-@if(count($leaderRows[$key]??[])>3)
+@if(count($leaderRows[$key]??[])>$leaderLimit)
 <details class="leader-more"><summary><span class="when-closed">View all {{ count($leaderRows[$key]) }} results</span><span class="when-open">Show fewer results</span></summary>
-@foreach(array_slice($leaderRows[$key],3,null,true) as $i=>$row)@include('partials.leader-row')@endforeach
+@foreach(array_slice($leaderRows[$key],$leaderLimit,null,true) as $i=>$row)@php $i += $leaderLimit; @endphp @include('partials.leader-row')@endforeach
 </details>
 @endif
 </article>
