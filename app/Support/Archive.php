0db3cd64cfe5832a58170832b2d44c87a8768e73
ecfhl <daniel.r.caissie@gmail.com>
2026-09-26 14:11:18 -0300

    Fix historical Sebastian Aho trade contracts

--- app/Support/Archive.php
+++ app/Support/Archive.php
@@ -142,9 +142,18 @@ class Archive extends EcfhlData
             }
             foreach (['from','to'] as $side) {
                 $years=array_replace($contracts[$side]??[], $verified[$t['id']][$t['season']][$side]??[]);
-                // Known 2025-26 Nov 20 trade: Sebastian Aho sent by Orcas had 2 years.
-                if ($t['season']==='2025-26' && str_contains((string)$t['date'], 'Nov 20, 2025') && $side==='to') {
-                    $years[TradeContracts::playerKey('Sebastian Aho')]=2;
+                // Historical Sebastian Aho contracts supplied from the league record.
+                if (TradeContracts::playerKey('Sebastian Aho')) {
+                    $date=(string)$t['date'];
+                    if ($t['season']==='2025-26' && str_contains($date, 'Nov 20, 2025') && $side==='to') {
+                        $years[TradeContracts::playerKey('Sebastian Aho')]=2;
+                    }
+                    if ($t['season']==='2022-23' && str_contains($date, 'Nov 22, 2022') && $side==='from') {
+                        $years[TradeContracts::playerKey('Sebastian Aho')]='FA';
+                    }
+                    if ($t['season']==='2021-22' && str_contains($date, 'Oct 21, 2021') && $side==='to') {
+                        $years[TradeContracts::playerKey('Sebastian Aho')]=2;
+                    }
                 }
                 foreach ($t[$side.'_items'] as &$text) {
                     $key=TradeContracts::playerKey($text);
